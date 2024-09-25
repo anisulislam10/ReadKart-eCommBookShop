279f69a2a06f5ba58290ed341101deb94e9bcda7
Anisul Islam <anis.inbox10@gmail.com>
2024-09-25 03:19:23 -0700

    added user address end point

--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, signInUser,getUserInformation } from "../controllers/user.controller.js";
+import { registerUser, signInUser,getUserInformation, updateAddress } from "../controllers/user.controller.js";
 import authenticateToken from '../middleware/userAuth.middleware.js';
 
 
@@ -7,8 +7,11 @@ import authenticateToken from '../middleware/userAuth.middleware.js';
 const router = express.Router();
 
  router.route("/signup").post(registerUser);
- router.route("/signin").post(signInUser)
- router.route("/getuser").get(authenticateToken,getUserInformation)
+ router.route("/signin").post(signInUser);
+ router.route("/getuser").get(authenticateToken,getUserInformation);
+ router.route("/updateaddress").post(updateAddress);
+
+
 
  export default router;
  
