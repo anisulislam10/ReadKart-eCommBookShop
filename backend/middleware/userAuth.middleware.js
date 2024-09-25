8be7ea6e6fb456f8a7e029557e096b043e9bfcd0
Anisul Islam <anis.inbox10@gmail.com>
2024-09-25 02:17:13 -0700

    added middleware for token autorization

---
+++ backend/middleware/userAuth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+
+dotenv.config({});
+
+const authenticateToken=(req,res,next)=>{
+    const authHeader=req.headers["authorization"];
+    const token=authHeader && authHeader.split(" ")[1];
+    console.log("middleware authtoken hitted");
+    console.log(token);
+    
+    
+
+    if(token==null){
+        return res.status(401).json({
+            message: "Authentication Token Required"
+        });
+        
+        
+    }
+    jwt.verify(token,process.env.SECRET_KEY, (err,user)=>{
+        if(err){
+        return res.status(403).json({
+            message:"Token Expired. Please sign-in again"
+        });
+    } 
+
+    req.user = user;    
+    next();
+    });
+}
+export default authenticateToken;
