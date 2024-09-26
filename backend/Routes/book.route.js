b02fe1db8ebaac63ea7b34e6c9a5cd5aa51bc5bc
Anisul Islam <anis.inbox10@gmail.com>
2024-09-25 23:31:00 -0700

    added routes for books

---
+++ backend/Routes/book.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import { addBookAdmin, updateBookAdmin,getBook,deleteBookAdmin,getRecentBooks,getBookbyId } from "../controllers/book.controller.js";
+import authenticateToken from "../middleware/userAuth.middleware.js";
+
+const router=express.Router()
+
+router.route("/addbook").post(authenticateToken,addBookAdmin)
+router.route("/updatebook").put(authenticateToken,updateBookAdmin)
+router.route("/getbook").get(authenticateToken,getBook)
+router.route("/deletebook").delete(authenticateToken,deleteBookAdmin)
+router.route("/getrecentbook").get(getRecentBooks)
+router.route("/getbookbyid/:id").get(getBookbyId)
+
+
+
+
+
+
+
+export default router;
