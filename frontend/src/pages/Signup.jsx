8360912627c6bd2cf54b9933a18557c8aeb0032a
Anisul Islam <anis.inbox10@gmail.com>
2024-10-06 13:58:17 +0500

    api intergeration with backend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,64 +1,86 @@
 import React, { useState } from 'react';
 import { SiGnuprivacyguard } from 'react-icons/si';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
 function Signup() {
   const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    address: ''
+    username: "",
+    useremail: "",
+    password: "",
+    address: ""
   });
-
+  const navigate=useNavigate();
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData); // Submit the form data
+  const handleSubmit = async () => {
+    // console.log(handleSubmit);
+    
+    try {
+      if (
+        formData.useremail === "" ||
+        formData.password === "" ||
+        formData.username === "" ||
+        formData.address === ""
+      ) {
+        alert("All Fields are Required")
+      }
+      else {
+        const response= await axios.post("http://localhost:3000/api/v1/user/signup", formData);
+        
+        alert(response.data.message);
+        
+        // console.log(formData)
+        navigate("/Sign-In")
+
+      }
+    } catch (error) {
+      console.log("Error response:", error.response.data.message);
+      
+    }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-purple-800">
       <div className="bg-white p-8 rounded shadow-md w-96">
 
-<div className='grid grid-flow-col items-center'>
-<SiGnuprivacyguard className='w-9 h-9'/>
-<h2 className="text-2xl font-bold mb-6 items-center mr-[180px] mt-6" >Sign Up</h2>
+        <div className='grid grid-flow-col items-center'>
+          <SiGnuprivacyguard className='w-9 h-9' />
+          <h2 className="text-2xl font-bold mb-6 items-center mr-[180px] mt-6" >Sign Up</h2>
+        </div>
 
-</div>
-
-
-        <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="space-y-4">
           <div>
-            <label htmlFor="username" className="block mb-2">Username</label>
+            <p className="block mb-2">Username</p>
             <input
               type="text"
               id="username"
               name="username"
-            placeholder='username'
+              placeholder='username'
               value={formData.username}
               onChange={handleChange}
               className="w-full px-4 py-2 border-2 rounded"
-              required
+            // required
             />
           </div>
           <div>
-            <label htmlFor="email" className="block mb-2">Email</label>
+            <p  className="block mb-2">Email</p>
             <input
               type="email"
-              id="email"
-              name="email"
+              id="useremail"
+              name="useremail"
               placeholder='email@domain.com'
-              value={formData.email}
+              value={formData.useremail}
               onChange={handleChange}
               className="w-full px-4 py-2 border rounded"
-              required
+            // required
             />
           </div>
           <div>
-            <label htmlFor="password" className="block mb-2">Password</label>
+            <p className="block mb-2">Password</p>
             <input
               type="password"
               id="password"
@@ -67,11 +89,11 @@ function Signup() {
               value={formData.password}
               onChange={handleChange}
               className="w-full px-4 py-2 border rounded"
-              required
+            // required
             />
           </div>
           <div>
-            <label htmlFor="address" className="block mb-2">Address</label>
+            <p className="block mb-2">Address</p>
             <input
               type="text"
               id="address"
@@ -80,13 +102,13 @@ function Signup() {
               value={formData.address}
               onChange={handleChange}
               className="w-full px-2 py-8 border rounded"
-              required
+            // required
             />
           </div>
-          <button type="submit" className="w-full bg-purple-800 text-white py-2 rounded mt-4 hover:bg-purple-900">
+          <button onClick={handleSubmit} className="w-full bg-purple-800 text-white py-2 rounded mt-4 hover:bg-purple-900">
             Sign Up
           </button>
-        </form>
+        </div>
         <p className="mt-4 text-center">
           Already have an account? <Link to="/Sign-In" className="text-purple-600">Sign in here</Link>
         </p>
