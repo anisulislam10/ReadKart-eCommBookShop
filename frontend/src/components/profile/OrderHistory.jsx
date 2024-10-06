a4125158fe0eff1ab5020283572daa17f76a4976
Anisul Islam <anis.inbox10@gmail.com>
2024-10-06 18:50:03 +0500

    added new pages for profile dashboard

---
+++ frontend/src/components/profile/OrderHistory.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function OrderHistory() {
+  return (
+    <div>
+
+<h1 className='text-2xl font-semibold'>Order History</h1>
+
+    </div>
+  )
+}
+
+export default OrderHistory
---
+++ frontend/src/components/profile/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { FaHeart, FaHistory } from 'react-icons/fa'
+import { LuLogOut, LuSettings } from 'react-icons/lu'
+import { Link } from 'react-router-dom'
+
+
+function Sidebar({ data }) {
+    const handleLogout=(e)=>{
+        alert("Logout Successfully");
+        
+    }
+    return (
+        <div className='bg-purple-900 p-4 rounded flex-col items-center justify-center h-[100%] shadow-lg'>
+            <div className='shadow-2xl rounded items-center text-center'>
+
+            <img src={data.avatar} alt={ `${data.username } Avatar`} className='h-[10vh]' />
+            <p className='text-xl font-semibold '>{data.username}</p>
+            <p className='text-x font-semibold'> {data.useremail}</p>
+            <div className='w-full mt-4 h-[1px] bg-slate-300'></div>
+
+            </div>
+
+
+            <div className='w-full h-full flex-col mt-4 '>
+
+                <div className='grid grid-flow-col hover:bg-purple-950 transition duration-300'>
+                <FaHeart className='mt-4 ml-4 ' /><Link to={"/profile"} className='w-full h-full  pt-3   font-semibold rounded hover:bg-purple-950 transition duration-300  text-left items-left justify-left pr-16 py-1'>Favourites </Link> <br />
+                </div>
+
+
+                <div className='grid grid-flow-col hover:bg-purple-950 transition duration-300'>
+                <FaHistory className='mt-4 ml-4 '/> <Link to={"/profile/history"} className='w-full h-full  pt-3  font-semibold rounded hover:bg-purple-950  transition duration-300 text-left items-left justify-left pr-11 py-1'>Order History </Link> <br />
+                </div>
+
+                <div className='grid grid-flow-col hover:bg-purple-950 transition duration-300'> 
+                <LuSettings className='mt-4 ml-4 '/> <Link to={"/profile/settings"} className='w-full h-full pt-3   font-semibold rounded hover:bg-purple-950  transition duration-300  text-left items-left justify-left pr-20 py-1' >Settings</Link> <br />
+                                
+
+                </div>
+
+                <button onClick={handleLogout} className='grid grid-flow-col hover:bg-purple-950 transition duration-300 pt-3 font-semibold  pr-[100px] py-1'>
+                    <LuLogOut className='mt-1 ml-4  '/> Logout
+                    </button><br />
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
